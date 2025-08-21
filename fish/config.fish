ecd2dab822207c5713fb274c2643305fabe0fe09
Gage Atkinson <someStranger87@gmail.com>
2025-08-21 14:54:55 -0400

    changed color scheme

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,27 @@
 
-source ~/.cache/wal/colors.fish
 
 function fish_greeting
-  source ~/.cache/wal/colors.fish
+	source ~/.cache/wal/colors.fish
+end
+
+function fish_prompt
+  
+  set -l color_cwd
+  set -l prefix
+  set -l suffix
+  set color_cwd $fish_color_cwd
+  set suffix '$'
+
+  # PWD
+  set_color $color2
+  echo -n (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (fish_vcs_prompt)
+
+  set_color normal
+
+  echo -n "$suffix "
 end
 
 alias nv "nvim"
