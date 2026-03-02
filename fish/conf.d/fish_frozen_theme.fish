6e0082ba0513797bb7fa64e4f881148d8370fabb
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2026-03-01 19:00:02 -0500

    Add files via upload

---
+++ fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,57 @@
+# This file was created by fish when upgrading to version 4.3, to migrate
+# theme variables from universal to global scope.
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+# See also the release notes for fish 4.3.0 (run `help relnotes`).
+
+set --global fish_color_autosuggestion 65525c
+set --global fish_color_cancel -r
+set --global fish_color_command 975BC2
+set --global fish_color_comment CB90B3
+set --global fish_color_cwd AE9AD5
+set --global fish_color_cwd_root red
+set --global fish_color_end 7F95E0
+set --global fish_color_error ff0000
+set --global fish_color_escape 7F95E0
+set --global fish_color_hg_added green
+set --global fish_color_hg_clean green
+set --global fish_color_hg_copied magenta
+set --global fish_color_hg_deleted red
+set --global fish_color_hg_dirty red
+set --global fish_color_hg_modified yellow
+set --global fish_color_hg_renamed magenta
+set --global fish_color_hg_unmerged red
+set --global fish_color_hg_untracked yellow
+set --global fish_color_history_current --bold
+set --global fish_color_host AE9AD5
+set --global fish_color_host_remote yellow
+set --global fish_color_keyword
+set --global fish_color_match --background=brblue
+set --global fish_color_normal normal
+set --global fish_color_operator 7F95E0
+set --global fish_color_option
+set --global fish_color_param CE66C8
+set --global fish_color_quote CB90B3
+set --global fish_color_redirection 9096DB
+set --global fish_color_search_match white --background=brblack
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status normal
+set --global fish_color_user AE9AD5
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_background
+set --global fish_pager_color_completion
+set --global fish_pager_color_description CB90B3 yellow
+set --global fish_pager_color_prefix white --bold --underline
+set --global fish_pager_color_progress brwhite --background=cyan
+set --global fish_pager_color_secondary_background
+set --global fish_pager_color_secondary_completion
+set --global fish_pager_color_secondary_description
+set --global fish_pager_color_secondary_prefix
+set --global fish_pager_color_selected_background -r
+set --global fish_pager_color_selected_completion
+set --global fish_pager_color_selected_description
+set --global fish_pager_color_selected_prefix
