e43c1fe77b3bf234e96d428c22c9e28004c5fbc5
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2026-07-20 20:45:39 -0400

    Fixed install script
    broken stuff cause im stupid

--- install.sh
+++ install.sh
@@ -1,18 +1,8 @@
 
-# install yay
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ..
-
-# install dotfiles
-git clone https://github.com/somestranger8/dotfiles
-cd dotfiles
 rm -rf Screenshots README.md
-mv Wallpapers ~
-mkdir ~/.config
-mv * ~/.config
+mv -f Wallpapers ~
+mkdir -p ~/.config
+mv -f * ~/.config
 
 # install packages
 yay -S hyprland ly alacritty awww waybar-cava dunst fish tofi matuwall hyprlock hyprshot ttf-hack-nerd ttf-jetbrains-mono-nerd ttf-apple-emoji uwsm neovim firefox discord spotify steam
